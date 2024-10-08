65acf150a1dc9b902064e1765d945a1cf28451d8
akihisa <sahikia117@gmail.com>
2024-10-08 18:14:56 +0900

    掲示板一覧画面と新規作成機能の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -1,9 +1,19 @@
 class BoardsController < ApplicationController
 	def index
+		@boards = Board.all
 	end
 
 	def new
 		@board = Board.new
-		binding.pry
+	end
+
+	def create
+		Board.create(board_params)
+		redirect_to action: :index
+	end
+
+	private
+	def board_params
+		params.require(:board).permit(:author_name, :title, :body)
 	end
 end
