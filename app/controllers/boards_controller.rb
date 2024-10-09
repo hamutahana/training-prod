5eb8414c44c67d042d71209811fca2fd33a15042
akihisa <sahikia117@gmail.com>
2024-10-09 11:23:32 +0900

    投稿削除機能の実装

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -8,8 +8,8 @@ class BoardsController < ApplicationController
 	end
 
 	def create
-		Board.create(board_params)
-		redirect_to action: :index
+		board = Board.create(board_params)
+		redirect_to board
 	end
 
 	def show
@@ -24,6 +24,12 @@ class BoardsController < ApplicationController
 		board = Board.find(params[:id])
 		board.update(board_params)
 		redirect_to board
+	end
+
+	def destroy
+		board = Board.find(params[:id])
+		board.delete
+		redirect_to boards_path
 	end
 
 	private
